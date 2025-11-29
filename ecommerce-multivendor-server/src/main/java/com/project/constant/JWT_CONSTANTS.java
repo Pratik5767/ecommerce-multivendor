8b2715fc8d88b0dd5b839cee60fbb347e96f5aa2
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-11-29 14:53:09 +0530

    resolve issues

--- ecommerce-multivendor-server/src/main/java/com/project/constant/JWT_CONSTANTS.java
+++
@@ -1,7 +0,0 @@
-package com.project.constant;
-
-public class JWT_CONSTANTS {
-    public static final String SECRET_KEY = "my_secret_key_is_ecommerce_multivendor_project_creation";
-    public static final String JWT_HEADER = "Authorization";
-
-}
