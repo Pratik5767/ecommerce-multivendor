175643ed545517f8034e5fe02617ffe88c0cb20e
Pratik5676 <pratikvsalunkhe924@gmail.com>
2025-12-02 19:40:20 +0530

    implemented custom exceptions

---
+++ ecommerce-multivendor-server/src/main/java/com/project/exception/GlobalException.java
@@ -0,0 +1,31 @@
+package com.project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(SellerException.class)
+    public ResponseEntity<ErrorDetails> sellerExceptionHandler(SellerException sc, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setError(sc.getMessage());
+        errorDetails.setDetails(request.getDescription(false));
+        errorDetails.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ProductException.class)
+    public ResponseEntity<ErrorDetails> productExceptionHandler(ProductException pc, WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails();
+        errorDetails.setError(pc.getMessage());
+        errorDetails.setDetails(request.getDescription(false));
+        errorDetails.setTimestamp(LocalDateTime.now());
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ ecommerce-multivendor-server/src/main/java/com/project/exception/SellerException.java
@@ -0,0 +1,9 @@
+package com.project.exception;
+
+public class SellerException extends Exception {
+
+    public SellerException(String message) {
+        super(message);
+    }
+
+}
